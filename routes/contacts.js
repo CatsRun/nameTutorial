3808cd2e6564153ef188b35d3fd35820d027111c
Angela Murray <angelamurray776@outlook.com>
2026-02-02 18:30:03 -0500

    added post, put delete

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,10 +3,19 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+// get all contacts
 router.get('/', contactsController.getAll);
 
+// get single contact by id
 router.get('/:id', contactsController.getSingle);
 
-// add more routes here for post, put, delete, 
+// create new contact (post can be used for create or update depending on the function)
+router.post('/', contactsController.createContact);
+
+// update contact by id, replaces old data
+router.put('/:id', contactsController.updateContact);   
+
+// delete contact by id
+router.delete('/:id', contactsController.deleteContact);  
 
 module.exports = router;
