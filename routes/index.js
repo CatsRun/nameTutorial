a4edfb114e1b18fd5893966872d5ca94846f2770
Angela Murray <angelamurray776@outlook.com>
2026-02-02 13:51:20 -0500

    added router.rest

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// router.use('/'); //root route -- how do I make this work? 
 router.use('/', require('./swagger')); //added for swagger documentation route
 router.use('/contacts', require('./contacts'))
 
--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 const app = express();
 
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./routes/swagger');
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
